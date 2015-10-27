cb70f5331ba41d2dc471e3ee7a8adc1503412cdb
dchen71 <dchen12812@gmail.com>
2015-10-27 15:02:19 -0700

    convert na poster value to na image

--- Scripts/controllers/MainController.js
+++ Scripts/controllers/MainController.js
@@ -1,6 +1,7 @@
-app.controller('MainController', ['$scope', 'movies', '$http', function($scope, movies, $http) {
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	$scope.titles = [];
 
+	//Gets the title information based in imdbID
 	function get_title(id){
 		var promise = $http.get('http://www.omdbapi.com/?i=' + id + '&r=json') 
             .success(function(data) { 
@@ -9,6 +10,7 @@ app.controller('MainController', ['$scope', 'movies', '$http', function($scope, 
         return promise;
 	}
 
+	//Returns JSON promise which contains title and imdbID for future query
 	function getMovies(title){
 		return $http.get('http://www.omdbapi.com/?s=' + title + '&type=movie&r=json') 
 		          .success(function(data) { 
@@ -19,12 +21,18 @@ app.controller('MainController', ['$scope', 'movies', '$http', function($scope, 
 		          }); 
 	}
 
+	//On click function which searches for movies based on input and creates series of promises to find title information
  	$scope.search = function(){
  		$scope.titles = [];
  		$scope.movies = getMovies($scope.movie.title);
+ 		//Promise to use values from prior promise to search for movie info based on imdbID
  		Promise.resolve($scope.movies).then(function(value){
  			for(var i = 0; i < value.data.Search.length; i++){
  				$scope.title = get_title(value.data.Search[i].imdbID).then(function(response){
+ 					//If poster is na switch to na picture
+ 					if(response.data.Poster == 'N/A'){
+ 						response.data.Poster = 'Images/NA.png';
+ 					}
  					return(response.data);
  				});
  				$scope.titles.push($scope.title);
